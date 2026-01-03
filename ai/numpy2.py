61b05be0c758653dbdd39ff32a8122e753f4a3db
Goutham231879 <whygoutham@gmail.com>
2026-01-03 22:53:02 +0530

    num2

---
+++ ai/numpy2.py
@@ -0,0 +1,29 @@
+import numpy as np 
+"""
+
+print(a , a.shape)
+
+print(a[0][0])
+print(a[2,2])
+
+print(a[-1][0])
+i=0
+np.where(a > 3 , a ,0 )
+print(a)
+print(a[[0,2],[1,2]])
+a = np.array(
+    [1,2,3]
+)
+b = np.array(
+    [1,2,3]
+)
+
+c = a+b
+print(c)
+a = np.array([1,2,3])
+b = 10
+
+c = a+b
+print(c)
+"""
+
