810c1b25941b4788b9d0ded2de2ca29d00c5b3c2
Goutham231879 <whygoutham@gmail.com>
2026-01-13 16:26:07 +0530

    hm

---
+++ code/main.js
@@ -0,0 +1,16 @@
+/*
+
+
+function add(a : number , b :number ) : number {
+    return a+b
+}
+
+console.log(add(2,3))
+function echo0(msg) {
+    console.log(msg.toUpperCase());
+}
+echo0("goutham");
+
+message.tolower()
+message()
+*/
--- code/main.ts
+++ code/main.ts
@@ -1,2 +1,20 @@
-let mg : string = "hi goutham";
-console.log(mg)
+/*
+
+
+function add(a : number , b :number ) : number {
+    return a+b
+}
+
+console.log(add(2,3))
+
+function echo0 (msg : String) : void {
+    console.log(msg.toUpperCase())
+}
+
+
+echo0("goutham")
+*/
+
+
+
+
