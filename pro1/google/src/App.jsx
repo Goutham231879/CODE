6fa70b5d1887a969da8901d1147e43ab12c2f49f
Goutham231879 <whygoutham@gmail.com>
2026-01-27 21:37:07 +0530

    pro1

---
+++ pro1/google/src/App.jsx
@@ -0,0 +1,16 @@
+import React  from "react"
+
+import Navbar from  './components/Navbar'
+import Footer from './components/Footer'
+import Routes from './components/Routes'
+
+export default function App() {
+
+  return (
+    <div>
+      <Navbar />
+      <h1>goutham</h1>
+    </div>
+  );
+  
+}
