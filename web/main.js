c350e0fdbe3b12e01ec18d04d22d5743ef63a599
Goutham231879 <whygoutham@gmail.com>
2026-02-01 18:46:15 +0530

    feb1

--- web/main.js
+++ web/main.js
@@ -1,6 +1,26 @@
-const app = document.getElementById("app");
-const header = document.createElement("h1");
-const text = " goutha is what the fck";
-const headerContent =  document.createTextNode(text);
-header.appendChild(headerContent);
-app.appendChild(header);
+
+const React = window.React;
+const ReactDOM = window.ReactDOM;
+
+
+function Name(){
+    return  <h1>goutham</h1>;
+}
+
+function Want(){
+    return <h2>Money</h2>
+}
+
+
+function Oneui(){
+    return <>
+     <h1>what is my name</h1>
+     <Name/>
+     <Want/>
+    </>
+}
+
+
+const app = document.getElementById('app');
+const root = ReactDOM.createRoot(app);
+root.render( <Oneui/>);
