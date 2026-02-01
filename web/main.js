ceb674968864c6695a0541fd198c634b237c7e9d
Goutham231879 <whygoutham@gmail.com>
2026-02-01 18:21:45 +0530

    message

---
+++ web/main.js
@@ -0,0 +1,6 @@
+const app = document.getElementById("app");
+const header = document.createElement("h1");
+const text = " goutha is what the fck";
+const headerContent =  document.createTextNode(text);
+header.appendChild(headerContent);
+app.appendChild(header);
